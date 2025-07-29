495c3503f19629b44eca32a8ae5a92626fc65b7c
Geovannni Almeida <131813766+gezinnn@users.noreply.github.com>
2025-07-29 19:05:14 -0300

    feat: add Header Component and Banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-import { Button } from "@/components/ui/button";
+import Header from "@/components/header"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 
 export default function Home() {
   return (
     <div>
-      <Button>Teste</Button>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Geovanni!</h2>
+        <p>Terça-feira, 29 de Julho</p>
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Search" />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner.png"
+            alt="Banner FSW"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+        <div></div>
+      </div>
     </div>
-  );
+  )
 }
